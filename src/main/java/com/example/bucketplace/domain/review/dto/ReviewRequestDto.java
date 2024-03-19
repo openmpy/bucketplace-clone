fe9b3c15934de9178342d4a67d2d721cc66937ee
yuha <yhj230508@gmail.com>
2024-03-19 20:55:17 +0900

    feat: 리뷰 초기 설정(#8)

--- src/main/java/com/example/bucketplace/domain/review/dto/ReviewRequestDto.java
+++ src/main/java/com/example/bucketplace/domain/review/dto/ReviewRequestDto.java
@@ -1,4 +1,27 @@
 package com.example.bucketplace.domain.review.dto;
 
+import com.example.bucketplace.domain.review.entity.Review;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.PositiveOrZero;
+import lombok.Getter;
+
 public class ReviewRequestDto {
+
+    @Getter
+    public static class CreateReviewRequestDto {
+
+        @NotBlank(message = "리뷰를 입력해주세요.")
+        private String contents;
+
+        @PositiveOrZero(message = "별점을 입력해주세요.")
+        private int rating;
+
+        public Review toEntity() {
+            return Review.builder()
+                    .contents(this.contents)
+                    .rating(this.rating)
+                    .build();
+        }
+
+    }
 }
--- src/main/java/com/example/bucketplace/domain/review/dto/ReviewResponseDto.java
+++ src/main/java/com/example/bucketplace/domain/review/dto/ReviewResponseDto.java
@@ -1,4 +1,19 @@
 package com.example.bucketplace.domain.review.dto;
 
+import com.example.bucketplace.domain.review.entity.Review;
+import lombok.Getter;
+
 public class ReviewResponseDto {
+
+    @Getter
+    public static class CreateReviewResponseDto {
+
+        private String contents;
+        private int rating;
+
+        public CreateReviewResponseDto(Review review) {
+            this.contents = review.getContents();
+            this.rating = review.getRating();
+        }
+    }
 }

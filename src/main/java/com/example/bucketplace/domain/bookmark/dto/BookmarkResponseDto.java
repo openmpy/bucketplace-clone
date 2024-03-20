9954481e51eac4579c3d0d2dee6fba21d4a8b9e8
김수환 <suhwan@kakao.com>
2024-03-20 11:48:36 +0900

    feat: 북마크 등록 기능 추가 (#23)

--- src/main/java/com/example/bucketplace/domain/bookmark/dto/BookmarkResponseDto.java
+++ src/main/java/com/example/bucketplace/domain/bookmark/dto/BookmarkResponseDto.java
@@ -1,4 +1,24 @@
 package com.example.bucketplace.domain.bookmark.dto;
 
+import com.example.bucketplace.domain.bookmark.entity.Bookmark;
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
 public class BookmarkResponseDto {
+
+    @Getter
+    public static class CreateBookmarkResponseDto {
+
+        private final Long productId;
+
+        @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss", timezone = "Asia/Seoul")
+        private final LocalDateTime createdAt;
+
+        public CreateBookmarkResponseDto(Bookmark bookmark) {
+            this.productId = bookmark.getProduct().getId();
+            this.createdAt = bookmark.getCreatedAt();
+        }
+    }
 }

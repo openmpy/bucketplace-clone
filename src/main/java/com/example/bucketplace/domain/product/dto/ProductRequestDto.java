d3027eee7d2780c997fa5497f1820e30a5b6e523
Peter-Yu-0402 <149031461+Peter-Yu-0402@users.noreply.github.com>
2024-03-20 18:01:20 +0900

    feat: 전체 상품 조회 기능 (#28)
    * feat: 전체 상품 조회 기능
    
    * feat: 전체 상품 조회 Swagger 추가
    
    ProductControllerDocs implement 누락된 점 보충하였습니다.
    
    * fix: 전체 상품 조회 Boolean 수정
    
    boolena -> Boolean
    
    * feat: 전체 상품 조회 permitAll 추가
    
    boolena -> Boolean
    
    * feat: 전체 상품 조회 permitAll 추가
    
    * style: 불명확, 부정확한 타이핑 수정
    
    * style: 불필요하게 긴 코드, 비직관적 명칭, 띄어쓰기 수정

--- src/main/java/com/example/bucketplace/domain/product/dto/ProductRequestDto.java
+++ src/main/java/com/example/bucketplace/domain/product/dto/ProductRequestDto.java
@@ -1,4 +1,14 @@
 package com.example.bucketplace.domain.product.dto;
 
+import lombok.Getter;
+
+@Getter
 public class ProductRequestDto {
+    private String brand;
+    private String name;
+    private double discount;
+    private long price;
+    private String imageUrl;
+    private boolean isFreeDelivery;
+    private boolean isSpecialPrice;
 }

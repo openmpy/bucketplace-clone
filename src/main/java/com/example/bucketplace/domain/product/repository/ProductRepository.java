7014ab0aa2f64b7bc2381b06a472cd4de229e573
openmpy <suhwan@kakao.com>
2024-03-20 20:31:32 +0900

    feat: 상품 크롤링 기능 추가

--- src/main/java/com/example/bucketplace/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/bucketplace/domain/product/repository/ProductRepository.java
@@ -4,4 +4,6 @@ import com.example.bucketplace.domain.product.entity.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByName(String name);
 }

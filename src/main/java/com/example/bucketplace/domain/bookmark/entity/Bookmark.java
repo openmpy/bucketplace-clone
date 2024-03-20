a54962f316745208f87345286e7e480b1c72ab1c
김수환 <suhwan@kakao.com>
2024-03-20 12:09:26 +0900

    feat: 북마크 삭제 기능 추가 (#24)
    * feat: 북마크 삭제 기능 추가
    
    * feat: 북마크 삭제 기능 Swagger 적용

--- src/main/java/com/example/bucketplace/domain/bookmark/entity/Bookmark.java
+++ src/main/java/com/example/bucketplace/domain/bookmark/entity/Bookmark.java
@@ -8,16 +8,22 @@ import lombok.AccessLevel;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import org.hibernate.annotations.SQLDelete;
+import org.hibernate.annotations.Where;
 
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
 @Table(name = "bookmark_tbl")
+@SQLDelete(sql = "UPDATE bookmark_tbl SET deleted = true WHERE id = ?")
+@Where(clause = "deleted = false")
 public class Bookmark extends Timestamped {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private boolean deleted = Boolean.FALSE;
 
     @ManyToOne(fetch = FetchType.LAZY)
     @JoinColumn(name = "member_id", nullable = false)
@@ -28,7 +34,8 @@ public class Bookmark extends Timestamped {
     private Product product;
 
     @Builder
-    public Bookmark(Member member, Product product) {
+    public Bookmark(boolean deleted, Member member, Product product) {
+        this.deleted = deleted;
         this.member = member;
         this.product = product;
     }

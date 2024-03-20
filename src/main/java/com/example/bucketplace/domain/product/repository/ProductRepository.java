106e7393d64ce4002d4f84aea457511caa79eece
김수환 <suhwan@kakao.com>
2024-03-20 20:56:21 +0900

    feat: 상품 크롤링 기능 추가 (#36)

--- src/main/java/com/example/bucketplace/domain/product/repository/ProductRepository.java
+++ src/main/java/com/example/bucketplace/domain/product/repository/ProductRepository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    boolean existsByName(String name);
     Optional<Product> findById(Long id);
 }
---
+++ src/main/java/com/example/bucketplace/domain/product/scheduler/ProductScheduler.java
@@ -0,0 +1,115 @@
+package com.example.bucketplace.domain.product.scheduler;
+
+import com.example.bucketplace.domain.product.entity.Product;
+import com.example.bucketplace.domain.product.repository.ProductRepository;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import lombok.extern.slf4j.Slf4j;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+import java.time.Duration;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Component
+public class ProductScheduler {
+
+    private static final String BUCKETPLACE_SHOP_URL = "https://ohou.se/store";
+    private final ProductRepository productRepository;
+
+    public ProductScheduler(ProductRepository productRepository) {
+        this.productRepository = productRepository;
+    }
+
+    @Scheduled(fixedDelay = 60 * 60 * 1000) // 한 시간 마다 반복
+    public void bucketplaceCrawling() throws InterruptedException {
+        WebDriver webDriver = setupWebDriver();
+        try {
+            webDriver.get(BUCKETPLACE_SHOP_URL);
+            Thread.sleep(3000); // 페이지 로드 대기
+
+            List<Product> products = crawlProducts(webDriver);
+            productRepository.saveAll(products);
+        } finally {
+            webDriver.close();
+            webDriver.quit();
+        }
+        log.info("오늘의집 크롤링 성공");
+    }
+
+    private WebDriver setupWebDriver() {
+        WebDriverManager.chromedriver().setup();
+        ChromeOptions chromeOptions = new ChromeOptions();
+        chromeOptions.addArguments("--headless", "--no-sandbox", "--disable-dev-shm-usage");
+        return new ChromeDriver(chromeOptions);
+    }
+
+    private List<Product> crawlProducts(WebDriver webDriver) throws InterruptedException {
+        List<Product> productList = new ArrayList<>();
+        JavascriptExecutor javascriptExecutor = (JavascriptExecutor) webDriver;
+        WebDriverWait wait = new WebDriverWait(webDriver, Duration.ofSeconds(5));
+
+        for (int i = 0; i < 5; i++) {
+            javascriptExecutor.executeScript("window.scrollBy(0, 5000)");
+            Thread.sleep(3000);
+
+            List<WebElement> elementList = webDriver.findElements(By.cssSelector(".production-item"));
+            for (WebElement webElement : elementList) {
+                Product product = createProductFromWebElement(webElement, wait);
+                if (product != null) {
+                    productList.add(product);
+                }
+            }
+        }
+        return productList;
+    }
+
+    private Product createProductFromWebElement(WebElement webElement, WebDriverWait wait) {
+        String productName = webElement.findElement(By.cssSelector(".production-item__header__name")).getText();
+
+        if (productRepository.existsByName(productName)) {
+            return null;
+        }
+
+        String brandName = webElement.findElement(By.cssSelector(".production-item__header__brand")).getText();
+
+        Optional<WebElement> discountElement = safelyFindElement(webElement, By.cssSelector(".production-item-price__rate"));
+        String discount = discountElement.map(WebElement::getText).orElse("0%");
+
+        String price = webElement.findElement(By.cssSelector(".production-item-price__price")).getText().replace(",", "");
+
+        boolean isFreeDelivery = safelyFindElement(webElement, By.xpath("//*[@aria-label='무료배송']")).isPresent();
+        boolean isSpecialPrice = safelyFindElement(webElement, By.xpath("//*[@aria-label='특가']")).isPresent();
+
+        WebElement imageElement = wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".image")));
+        String imageUrl = imageElement.getAttribute("src");
+
+        return Product.builder()
+                .brand(brandName)
+                .name(productName)
+                .discount(Double.parseDouble(discount.replace("%", "")))
+                .price(Long.parseLong(price))
+                .imageUrl(imageUrl)
+                .isFreeDelivery(isFreeDelivery)
+                .isSpecialPrice(isSpecialPrice)
+                .build();
+    }
+
+    private Optional<WebElement> safelyFindElement(WebElement webElement, By by) {
+        try {
+            return Optional.ofNullable(webElement.findElement(by));
+        } catch (Exception e) {
+            return Optional.empty();
+        }
+    }
+}

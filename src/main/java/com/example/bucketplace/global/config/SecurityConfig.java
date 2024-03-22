e449e8f4da7f36425723953f9d95bd8ea35d18c0
openmpy <suhwan@kakao.com>
2024-03-22 22:02:15 +0900

    fix: add origins vercel test url

--- src/main/java/com/example/bucketplace/global/config/SecurityConfig.java
+++ src/main/java/com/example/bucketplace/global/config/SecurityConfig.java
@@ -102,7 +102,8 @@ public class SecurityConfig {
         configuration.setAllowedOrigins(
                 List.of(
                         "http://localhost:3000", "http://localhost:5000", "http://localhost:5500",
-                        "http://127.0.0.1:3000", "http://127.0.0.1:5000", "http://127.0.0.1:5500"
+                        "http://127.0.0.1:3000", "http://127.0.0.1:5000", "http://127.0.0.1:5500",
+                        "https://js-teal.vercel.app"
                 )
         );
         configuration.setAllowCredentials(true);

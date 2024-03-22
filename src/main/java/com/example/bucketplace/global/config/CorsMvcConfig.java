131f4d830900ae3f065b0a5b7336a6b83db24fb1
openmpy <suhwan@kakao.com>
2024-03-22 22:46:28 +0900

    fix: edit cookie setting

--- src/main/java/com/example/bucketplace/global/config/CorsMvcConfig.java
+++ src/main/java/com/example/bucketplace/global/config/CorsMvcConfig.java
@@ -14,7 +14,8 @@ public class CorsMvcConfig implements WebMvcConfigurer {
                 .exposedHeaders("Set-Cookie")
                 .allowedOrigins(
                         "http://localhost:3000", "http://localhost:5500",
-                        "http://127.0.0.1:3000", "http://127.0.0.1:5500"
+                        "http://127.0.0.1:3000", "http://127.0.0.1:5500",
+                        "https://js-teal.vercel.app"
                 );
     }
 }
